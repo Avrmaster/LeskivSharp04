d554af6888cf5295ada6fb78249c18fb229a2bdf
Oleksandr Leskiv <sasha26.05.2012@gmail.com>
2018-03-18 03:20:56 +0200

    Initial commit

---
+++ LeskivSharp04/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace LeskivSharp04
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new PersonRegisterViewModel(this);
+        }
+    }
+}
